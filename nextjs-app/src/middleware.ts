75049d5b8b96807ac4fca97313725f119beb07f9
royleej9 <royleej9@gmail.com>
2025-03-26 16:49:48 +0900

    인증 체크

--- nextjs-app/src/middleware.ts
+++ nextjs-app/src/middleware.ts
@@ -4,24 +4,30 @@ import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import { pagePath, PARAM_CALLBAK_URL } from './shared/lib/path';
 
-const noneRequiredAuthPaths: readonly string[] = ['/', '/login', '/signup'];
+// 인증 필요
+const protectedRoutes: readonly string[] = [
+  pagePath.dashboard(),
+  pagePath.main(),
+];
+
+// 인증 필요 없음
+const publicRoutes: readonly string[] = [pagePath.logn()];
 
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  console.log(`요청 path: ${path}`);
-
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
   const sessionCookie = (await cookies()).get('session')?.value;
-  const isNoneRequiredAuthPath = noneRequiredAuthPaths.includes(path);
 
-  // 인증이 필요한 페이지 접속시 세션이 없을 때
-  if (!isNoneRequiredAuthPath && !sessionCookie) {
+  // 인증이 필요한 페이지 접속시 세션이 없을 때 > 로그인 페이지로 이동
+  if (isProtectedRoute && !sessionCookie) {
     const loginUrl = new URL(pagePath.logn(), req.url);
     loginUrl.searchParams.set(PARAM_CALLBAK_URL, req.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
   }
 
   // 인증이 필요하지 않은 페이지 접속시 세션이 있을 때
-  if (isNoneRequiredAuthPath && sessionCookie) {
+  if (isPublicRoute && sessionCookie) {
     return NextResponse.redirect(new URL(pagePath.dashboard(), req.nextUrl));
   }
 
