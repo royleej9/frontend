a0c0e4b78175b0248b92d94627a6fa077ea14c36
royleej9 <royleej9@gmail.com>
2025-03-27 21:45:34 +0900

    test11

--- nextjs-app/test.ts
+++ nextjs-app/test.ts
@@ -1,19 +1,27 @@
 const a = {
-  b: 'cc',
-  c: 'dd',
-};
+    b: "cc",
+    c: "dd"
+}
 
 const b = {
-  b: 'cc',
-  c: 'dd',
-};
+    b: "cc",
+    c: "dd"
+}
+
+
+
 
 const d = {
-  b: 'cc',
-  c: 'dd',
-};
+  b: "cc",
+  c: "dd"
+}
 
 const e = {
-  b: 'cc',
-  c: 'dd',
-};
+  b: "cc",
+  c: "dd"
+}
+
+const f = {
+  b: "cc",
+  c: "dd"
+}
