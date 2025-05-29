99c620fce168fa5f4e35eb7a25d0082016c1c536
royleej9 <royleej9@gmail.com>
2025-05-29 22:47:05 +0900

    HMR 적용

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [
     // locatorjs 사용을 위해 변경
     // https://www.locatorjs.com/install/react-data-id?stack=Vite
