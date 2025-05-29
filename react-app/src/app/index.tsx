5f6a76da1a19d2ac1076fdcabeaf088c8f1fa49b
royleej9 <royleej9@gmail.com>
2025-05-29 19:04:28 +0900

    app 폴더 변경경

--- react-app/src/app/index.tsx
+++ react-app/src/app/index.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { LoginPage } from '../pages/login/login-page';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LoginPage />
   </StrictMode>
 );
